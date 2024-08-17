1a22536b5f2f3e10d29751235ebc61fcda97d59c
AnjanKumarS <anjankumars65432@gmail.com>
2024-08-17 22:10:29 +0530

    code pushed

--- AverageOfEvenNumber.java
+++ AverageOfEvenNumber.java
@@ -3,13 +3,14 @@ public class AverageOfEvenNumber {
         int[] a = { 1, 5, 8, 6, 4, 25, 36 };
         System.out.println(avgevenNum(a));
     }
+
     
     public static double avgevenNum(int[] a) {
         double sum = 0;
         int c = 0;
         for (int i = 0; i < a.length; i++) {
             if (a[i] % 2 == 0) {
-                sum = sum + a[i];
+                sum= sum + a[i];
                 c++;
             }
         }
